3d68aab222d053a2f685f82c5312f6d777a4a795
Mohamed Zeineldin <mohamed.zeineldin@gfk.com>
2019-02-25 00:30:44 +0100

    curl instead of wget to negelect changing the scraper file

--- script.sh
+++ script.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-wget  -qnv http://dojodevopschallenge.s3-website-eu-west-1.amazonaws.com/fortune_of_the_day.json  
-
 while true; do 
-grep message fortune_of_the_day.json |  cut -f 2 -d ":" | cut -d '"' -f2  | shuf -n 1 
-#> /usr/share/nginx/html/index.html
+curl -s   http://dojodevopschallenge.s3-website-eu-west-1.amazonaws.com/fortune_of_the_day.json | grep message fortune_of_the_day.json |  cut -f 2 -d ":" | cut -d '"' -f2  | shuf -n 1 > /opt/index.html
 sleep 10 
 done
