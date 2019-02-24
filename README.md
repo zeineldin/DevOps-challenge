0a78ead72088a934ec1b45b2f4c5a01d73dd3b79
Mohamed Zeineldin <mohamed.zeineldin@gfk.com>
2019-02-24 23:42:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+docker-compose up -d 
+
+check 127.0.0.1 port 80
+you will find a cote every 10 seconds
---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget  -qnv http://dojodevopschallenge.s3-website-eu-west-1.amazonaws.com/fortune_of_the_day.json  
+
+while true; do 
+grep message fortune_of_the_day.json |  cut -f 2 -d ":" | cut -d '"' -f2  | shuf -n 1 
+#> /usr/share/nginx/html/index.html
+sleep 10 
+done
